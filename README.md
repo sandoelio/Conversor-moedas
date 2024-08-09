e29e8b5cf47b37c74b14f18e465ae5ebfc657220
sandoelio <sandoelio@hotmail.com>
2024-08-08 21:19:33 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,18 @@
+### Requisitos:
+- Laravel 8
+- Php 8
+
+### Inicio 
+- Clone o projeto
+
+### Rode os comandos
+- comando composer install
+- php artisan key:generate
+- php artisan serve
+
+### Como funciona o projeto
+
+- Será exibido a pagina no qual a pessoa escolhe o valor em Real e converte na moeda desejada
+- O valor tem que ser acima de 1000
+- logo apos selecione o metodo de pagamento
+-  E veja o resultado
---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoedaController;
+use App\Http\Controllers\UsuarioController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::post('/acesso', [UsuarioController::class, 'index']);
+Route::post('/create-user', [UsuarioController::class, 'createUser']);
+Route::get('/acesso-nivel/{email}', function ($email) {
+    return view('acesso.validando', ['email' => $email]);
+});
+
+Route::get('/',[MoedaController::class, 'listarMoedas']);
+Route::post('/converter',[MoedaController::class, 'converterMoeda']);
+Route::post('/pagamento', function(){
+    return view('main.pagamento');
+});
+
+Route::get('/register', function(){
+    return view('auth.register');
+});
+
+Route::get('/login', function(){
+    return view('auth.login');
+});
